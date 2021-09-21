9d19ef32354a607199e29776ae445f668b642bd6
Olga Seledets <olga.seledets@outlook.com>
2021-09-21 20:09:30 +0300

    Cosmetics

--- js/script.js
+++ js/script.js
@@ -16,18 +16,18 @@ window.addEventListener('DOMContentLoaded', function(){
             //let day = Math.floor(timeRemaining / 60 / 60 / 24);
 
             if(hours < 0) {
-                hours = '00';
-                minutes = '00';
-                seconds = '00';
+                hours = '0';
+                minutes = '0';
+                seconds = '0';
             }
 
-            if(hours < 10 && hours != 0) {
+            if(hours < 10) {
                 hours = '0'+hours;
             } 
-            if(minutes < 10 && minutes != 0) {
+            if(minutes < 10) {
                 minutes = '0'+minutes;
             } 
-            if(seconds < 10 && seconds != 0) {
+            if(seconds < 10) {
                 seconds = '0'+seconds;
             }
 
